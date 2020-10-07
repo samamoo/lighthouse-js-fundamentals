c1d3e093ce4515e9b0961d3638f10feab099decd
Samantha Fok <samantha.fok@hotmail.com>
2020-10-07 06:46:56 +0000

    Nothing to commit

--- odd.js
+++ odd.js
@@ -1,7 +1,7 @@
 var isOdd = function (num) {
   return num % 2 != 0;
 }
-console.log(isOdd(5));
+//console.log(isOdd(5));
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
